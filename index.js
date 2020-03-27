d9b8793c336b479ca88aaee4ad0a321ec5d052f5
alinecbsr <alinecbsr@gmail.com>
2020-03-27 15:06:48 -0300

    Route testing implemented using Insomnia, Nodemon installed as Dev dependency

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
   return response.json({
-    student: 'Aline'
+    student: 'Aline rosa',
+    name: 'Aline'
   });
 });
 
